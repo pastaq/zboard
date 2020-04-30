cae00e24ee198c82abe70efc583ebf7c37bb33f3
Derek J. Clark <derekjohn.clark@gmail.com>
2020-04-30 09:21:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+xmodmap keybindings for Ideazon Merc Zboard.
+Modified from ubuntu forums post by user Hex0x75.
+=================================================
+
+Usage
+-----
+
+Clone the repository
+
+$ chmod +x zboard.sh
+
+$ ./zboard.sh
+
---
+++ zboard.sh
@@ -0,0 +1,45 @@
+# red area (movement)
+xmodmap -e "keycode 181 = q"
+xmodmap -e "keycode 180 = w"
+xmodmap -e "keycode 225 = e"
+xmodmap -e "keycode 166 = a"
+xmodmap -e "keycode 136 = s"
+xmodmap -e "keycode 167 = d"
+
+# 1 to 11
+xmodmap -e "keycode 87 = 1"
+xmodmap -e "keycode 88 = 2"
+xmodmap -e "keycode 89 = 3"
+xmodmap -e "keycode 83 = 4"
+xmodmap -e "keycode 84 = 5"
+xmodmap -e "keycode 85 = 6"
+xmodmap -e "keycode 79 = 7"
+xmodmap -e "keycode 80 = 8"
+xmodmap -e "keycode 81 = 9"
+xmodmap -e "keycode 90 = 0"
+xmodmap -e "keycode 86 = minus"
+
+# save, load, printscreen
+xmodmap -e "keycode 137 = o"
+xmodmap -e "keycode 138= p"
+xmodmap -e "keycode 107 = l"
+
+# left area (score, voice)
+xmodmap -e "keycode 140 = Caps_Lock"
+xmodmap -e "keycode 148 = Tab"
+xmodmap -e "keycode 163 = Alt_L"
+xmodmap -e "keycode 106 = Shift_L"
+xmodmap -e "keycode 91 = Control_L"
+
+# right area (reload, use)
+xmodmap -e "keycode 164 = r"
+xmodmap -e "keycode 125 = f"
+xmodmap -e "keycode 194 = t"
+xmodmap -e "keycode 195 = g"
+xmodmap -e "keycode 196 = v"
+xmodmap -e "keycode 197 = b"
+xmodmap -e "keycode 142 = p"
+xmodmap -e "keycode 198 = c"
+xmodmap -e "keycode 63 = space"
+
+# [end] gamepad of keyboard (left)
